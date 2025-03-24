3c72b60a8906a7f217d4adfe3c909f7d27232e0b
Rod Christiansen <rchristiansen@ecuad.ca>
2025-03-23 20:25:38 -0700

    Update database configuration: add SSL options for MySQL connection

--- app/config/db.php
+++ app/config/db.php
@@ -14,22 +14,25 @@ switch ($driver) {
 
     case 'mysql':
         return [
-            'driver'    => 'mysql',
-            'host'      => env('CONNECTION_HOST', '127.0.0.1'),
-            'port'      => env('CONNECTION_PORT', 3306),
-            'database'  => env('CONNECTION_DATABASE', 'munkireport'),
-            'username'  => env('CONNECTION_USERNAME', 'munkireport'),
-            'password'  => env('CONNECTION_PASSWORD', 'munkireport'),
-            'charset'   => env('CONNECTION_CHARSET', 'utf8mb4'),
-            'collation' => env('CONNECTION_COLLATION', 'utf8mb4_unicode_ci'),
-            'strict'    => env('CONNECTION_STRICT', true),
-            'engine'    => env('CONNECTION_ENGINE', 'InnoDB'),
+            'driver'      => 'mysql',
+            'host'        => env('CONNECTION_HOST', '127.0.0.1'),
+            'port'        => env('CONNECTION_PORT', 3306),
+            'database'    => env('CONNECTION_DATABASE', 'munkireport'),
+            'username'    => env('CONNECTION_USERNAME', 'munkireport'),
+            'password'    => env('CONNECTION_PASSWORD', 'munkireport'),
+            'charset'     => env('CONNECTION_CHARSET', 'utf8mb4'),
+            'collation'   => env('CONNECTION_COLLATION', 'utf8mb4_unicode_ci'),
+            'strict'      => env('CONNECTION_STRICT', true),
+            'engine'      => env('CONNECTION_ENGINE', 'InnoDB'),
             'ssl_enabled' => env('CONNECTION_SSL_ENABLED', true),
-            'options'   => [
-                PDO::MYSQL_ATTR_SSL_CA    => env('CONNECTION_SSL_CA'),
+            'ssl_key'     => env('CONNECTION_SSL_KEY', null),
+            'ssl_cert'    => env('CONNECTION_SSL_CERT', null),
+            'ssl_ca'      => env('CONNECTION_SSL_CA', null),
+            'ssl_capath'  => env('CONNECTION_SSL_CAPATH', null),
+            'ssl_cipher'  => env('CONNECTION_SSL_CIPHER', null),
+            'options'     => [
+                PDO::MYSQL_ATTR_SSL_CA    => env('CONNECTION_SSL_CA', null),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('PDO_MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true),
-                PDO::MYSQL_ATTR_SSL_KEY   => env('CONNECTION_SSL_KEY', null),
-                PDO::MYSQL_ATTR_SSL_CERT  => env('CONNECTION_SSL_CERT', null),
             ],
         ];
 
